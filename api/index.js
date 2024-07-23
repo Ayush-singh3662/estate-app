acc4ade5c21e738216a133fb65478ac21a951349
Ayush <ayushsinghg610@gmail.com>
2024-07-23 21:03:17 +0530

    Connected DataBase

--- api/index.js
+++ api/index.js
@@ -1,6 +1,13 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
+
+mongoose.connect(process.env.MONGODB_URL)
+    .then(() => console.log('DataBase is connected successfully'))
+    .catch((err) => console.log(err));
 
 
 app.listen(3000, () => console.log('Server started at port 3000...'));

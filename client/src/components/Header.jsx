1118e5af9e0996efb3d99447232bb8f3f2f60602
Ayush <ayushsinghg610@gmail.com>
2024-07-26 13:19:50 +0530

    Added profile and made it private for authenticated user

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,9 @@
 import { FaSearch } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 const Header = () => {
+  const {currentUser} = useSelector(state => state.user);
   return (
     <header className="bg-slate-200 shadow-md">
         <div className="flex justify-between items-center mx-w-6xl mx-auto p-3">
@@ -18,7 +20,7 @@ const Header = () => {
           <ul className='flex gap-4'>
             <Link to='/'><li className='hidden sm:inline text-slate-700 hover:underline cursor-pointer'>Home</li></Link>
             <Link to='/about'><li className='hidden sm:inline text-slate-700 hover:underline cursor-pointer'>About</li></Link>
-            <Link to='/signin'><li className='text-slate-700 hover:underline cursor-pointer'>Sign In</li></Link>
+            <Link to='/profile'>{currentUser ? <img src={currentUser.avatar} alt='Profile' className='rounded-full h-9 w-9 object-cover' /> : <li className='text-slate-700 hover:underline cursor-pointer'>Sign In</li> }</Link>
           </ul>
         </div>
     </header>

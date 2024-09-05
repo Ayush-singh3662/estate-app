7d01afe1cf531d5f50b3e9d4a64e3665a4b6fef9
Ayush <ayushsinghg610@gmail.com>
2024-09-05 16:21:57 +0530

    Added pre installation for deployment

--- api/index.js
+++ api/index.js
@@ -5,10 +5,13 @@ import userRouter from "./routes/user.route.js";
 import signUpRouter from './routes/auth.route.js';
 import cookieParser from "cookie-parser";
 import createListingRouter from './routes/listing.route.js';
+import path from 'path';
 
 dotenv.config();
 
 const app = express();
+
+const __dirname = path.resolve();
 
 app.use(express.json());
 
@@ -25,6 +28,12 @@ app.get("/", (req, res) => res.send("Hii"));
 app.use("/api/user", userRouter);
 app.use('/api/auth', signUpRouter);
 app.use('/api/listing', createListingRouter);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
